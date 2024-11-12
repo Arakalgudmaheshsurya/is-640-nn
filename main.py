6645a869290eb0725fa42ffbf8bc1f3fcd178175
Arakalgudmaheshsurya <arakalgudmaheshsurya@gmail.com>
2024-11-11 17:21:27 -0800

    modified main.py part 2

--- main.py
+++ main.py
@@ -2,16 +2,13 @@
 from engine import Value
 from nn import MLP
 
+
 xs = [[2.0, 3.0, -1.0], 
       [3.0, -1.0, 0.5], 
       [0.5, 1.0, 1.0], 
-      [1.0, 1.0, -1.0]] 
+      [1.0, 1.0, -1.0]]  
 
-ys = [1.0, 1.0, 1.0, 1.0]  
-
-
-
-
+ys = [1.0, -1.0, -1.0, 1.0]  
 
 n = MLP(3, [4, 4, 1])
 
@@ -20,13 +17,15 @@ for k in range(20):
 
 
     ypred = [n(x) for x in xs]
+   
 
 
     loss = sum((yout - ygt) ** 2 for ygt, yout in zip(ys, ypred))
 
-
-
    
+
+
+
     for p in n.parameters():
         p.grad = 0.0
     loss.backward()  
@@ -37,8 +36,5 @@ for k in range(20):
 
     for p in n.parameters():
         p.data += -0.1 * p.grad 
-
-
-
 
     print(k, loss.data)
